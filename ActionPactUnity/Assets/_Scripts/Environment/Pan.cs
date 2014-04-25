796bf8609ff51c5a56d49d9ab1882fc7428f2dc6
Allan Lavell <alavell@gmail.com>
2014-04-25 17:04:10 -0400

    Add simple bg pan to convey sense of motion down tha mountain.

---
+++ ActionPactUnity/Assets/_Scripts/Environment/Pan.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pan : MonoBehaviour {
+	public float speed;
+	public Vector3 direction;
+
+	void Update() {
+		transform.position += speed * direction * Time.deltaTime;
+	}
+}
