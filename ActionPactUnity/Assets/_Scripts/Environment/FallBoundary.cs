ff419556e7092f7b82a682f66eb4d7697d8ce750
connorbell <connor.n.bell@gmail.com>
2014-04-27 15:00:55 -0400

    Jump off the edge yolo bug fix

--- ActionPactUnity/Assets/_Scripts/Environment/FallBoundary.cs
+++ ActionPactUnity/Assets/_Scripts/Environment/FallBoundary.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class FallBoundary : MonoBehaviour {
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerStay(Collider other) {
 		var shoppingCart = other.transform.parent.GetComponent<ShoppingCartBros> ();
 		if (shoppingCart) {
 			shoppingCart.Fall();
