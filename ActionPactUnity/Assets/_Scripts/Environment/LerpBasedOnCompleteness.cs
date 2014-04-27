dfec6d52885f761918e3cabaa38f58a879c013ce
Allan Lavell <alavell@gmail.com>
2014-04-27 12:11:02 -0400

    Add Lerp Based On Completeness to get the mountain ranges in the BG working invariant of the size f the mountain

---
+++ ActionPactUnity/Assets/_Scripts/Environment/LerpBasedOnCompleteness.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpBasedOnCompleteness : MonoBehaviour {
+	Vector3 from;
+	public Transform to;
+
+	void Start() {
+		from = transform.position;
+	}
+
+	void Update() {
+		transform.position = Vector3.Lerp (from, to.position, MountainGame.Instance.AmountComplete);
+	}
+}
