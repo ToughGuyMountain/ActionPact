2687eecf11debcf787cfa5e7fc251a92a7289381
connorbell <connor.n.bell@gmail.com>
2014-04-27 13:06:46 -0400

    RAMS NOW

---
+++ ActionPactUnity/Assets/_Scripts/Environment/Obstacles/Ram.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ram : Obstacle {
+
+	public void Start() {
+		float x = Random.Range(-3.0f, -1.0f);
+		float y = Random.Range(0.75f, 2.0f);
+		this.GetComponent<RelativePan>().direction = new Vector3(x, y, 0.0f); 
+	}
+
+	public override void ReturnToPool() {
+		RamPool.Instance.Push(this);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		var shoppingCartBros = other.transform.parent.GetComponent<ShoppingCartBros> ();
+		if (shoppingCartBros) {
+			shoppingCartBros.HitObstacle(this);
+		}
+	}
+}
---
+++ ActionPactUnity/Assets/_Scripts/Environment/Obstacles/RamPool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RamPool : Pool<Ram> {
+	
+	void Start() {
+		var spawner = GetComponent<Spawner> ();
+		spawner.SpawnWeight = () => 1;
+		spawner.Spawn = Spawn;
+	}	
+
+	public override void Spawn() {
+		var obj = Pop();
+		obj.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, -0.1f, Camera.main.nearClipPlane));
+		obj.transform.localPosition = new Vector3 (obj.transform.position.x, obj.transform.position.y, 0);
+	}
+}
