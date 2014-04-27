441e7853e06f33e8a9484465c2c20c704f564ced
Allan Lavell <alavell@gmail.com>
2014-04-26 23:33:25 -0400

    Add score GUI

---
+++ ActionPactUnity/Assets/_Scripts/UI/PointKeeper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointKeeper : MonoBehaviour {
+	public ShoppingCartBros bro;
+	UILabel label;
+
+	void Start () {
+		label = GetComponent<UILabel>();
+	}
+
+	void Update () {
+		label.text = bro.brewCount.ToString();
+	}
+}
